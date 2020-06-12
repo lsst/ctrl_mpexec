1f16c817d07905dc93aece77da42325901756d23
Nathan Pease <npease@slac.stanford.edu>
2020-06-11 19:00:23 -0500

    add a pipetask command

---
+++ python/lsst/ctrl/mpexec/cli/pipetask.py
@@ -0,0 +1,42 @@
+# This file is part of ctrl_mpexec.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import click
+
+from lsst.daf.butler.cli.butler import LoaderCLI
+from lsst.daf.butler.cli.opt import log_level_option
+
+
+class PipetaskCLI(LoaderCLI):
+
+    localCmdPkg = "lsst.ctrl.mpexec.cli.cmd"
+
+
+@click.command(cls=PipetaskCLI, context_settings=dict(help_option_names=["-h", "--help"]))
+@log_level_option()
+def cli(log_level):
+    # log_level is handled by get_command and list_commands, and is called in
+    # one of those functions before this is called.
+    pass
+
+
+def main():
+    return cli()
