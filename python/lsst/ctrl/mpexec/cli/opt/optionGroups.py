c47badc53903f9242ec1af1f3170e91cf040eb9a
Tim Jenness <tjenness@lsst.org>
2023-09-14 15:04:49 -0700

    Use a transfer option that does not include -t
    This stops it shadowing --task.

--- python/lsst/ctrl/mpexec/cli/opt/optionGroups.py
+++ python/lsst/ctrl/mpexec/cli/opt/optionGroups.py
@@ -40,7 +40,7 @@ __all__ = (
 import click
 import lsst.daf.butler.cli.opt as dafButlerOpts
 import lsst.pipe.base.cli.opt as pipeBaseOpts
-from lsst.daf.butler.cli.opt import transfer_option
+from lsst.daf.butler.cli.opt import transfer_option_no_short
 from lsst.daf.butler.cli.utils import OptionGroup, option_section, unwrap
 
 from . import options as ctrlMpExecOpts
@@ -112,7 +112,7 @@ class qgraph_options(OptionGroup):  # noqa: N801
             ctrlMpExecOpts.save_execution_butler_option(),
             ctrlMpExecOpts.clobber_execution_butler_option(),
             ctrlMpExecOpts.target_datastore_root_option(),
-            transfer_option(
+            transfer_option_no_short(
                 help=unwrap(
                     """Data transfer mode for the execution butler datastore.
                     Defaults to "copy" if --target-datastore-root is provided.
